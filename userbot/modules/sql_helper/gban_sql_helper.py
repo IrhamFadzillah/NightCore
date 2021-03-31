4d2eb134bc884beeaf75c58bda6f68dcb1eb476c
IrhamFadzillah <irham8381@gmail.com>
2021-04-01 02:07:14 +0700

    add module gban

--- userbot/modules/sql_helper/gban_sql_helper.py
+++ userbot/modules/sql_helper/gban_sql_helper.py
@@ -1,3 +1,17 @@
+"""
+credits to @mrconfused and @sandy1709
+"""
+#    Copyright (C) 2020  sandeep.n(π.$)
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from sqlalchemy import Column, String
 
 from userbot.modules.sql_helper import BASE, SESSION
@@ -32,13 +46,13 @@ def get_gbanuser(chat_id):
         SESSION.close()
 
 
-def freakgban(chat_id, reason):
+def catgban(chat_id, reason):
     adder = GBan(str(chat_id), str(reason))
     SESSION.add(adder)
     SESSION.commit()
 
 
-def freakungban(chat_id):
+def catungban(chat_id):
     rem = SESSION.query(GBan).get(str(chat_id))
     if rem:
         SESSION.delete(rem)
