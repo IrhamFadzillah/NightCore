369e6dc9fe273f3590bd5e49f73fc24f2e29a6d4
IrhamFadzillah <irham8381@gmail.com>
2021-04-02 01:44:44 +0700

    test fix

--- userbot/modules/gban.py
+++ userbot/modules/gban.py
@@ -75,8 +75,8 @@ async def global_ban(event):
             "`You need to be at least admin in 1 group to gban someone!`"
         )
     await event.edit(r"\\**#GBanned_User**//"
-                       f"\n\n**First Name:** [{user.first_name}](tg://user?id={user_id})\n"
-                       f"**User ID:** `{user_id}`\n**Reason:** `{reason}`")
+                       f"\n\n**First Name:** [{user.first_name}](tg://user?id={user.id})\n"
+                       f"**User ID:** `{user.id}`\n**Reason:** `{reason}`")
 
     
     for i in range(len(groups_admin)):
@@ -139,8 +139,8 @@ async def unglobal_ban(event):
             "`You need to be at least admin in 1 group to gban someone!`"
         )
     await event.edit(r"\\**#UnGbanned_User**//"
-                       f"\n\n**First Name:** [{user.first_name}](tg://user?id={user_id})\n"
-                       f"**User ID:** `{user_id}`")
+                       f"\n\n**First Name:** [{user.first_name}](tg://user?id={user.id})\n"
+                       f"**User ID:** `{user.id}`")
        
     for i in range(len(groups_admin)):
         try:
