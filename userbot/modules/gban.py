b56c60c8d9a1ce7154eacc80696fe4f11b663899
IrhamFadzillah <irham8381@gmail.com>
2021-04-01 15:28:54 +0700

    fix

--- userbot/modules/gban.py
+++ userbot/modules/gban.py
@@ -173,19 +173,6 @@ async def gablist(event):
                 )
     else:
         GBANNED_LIST = "no Gbanned Users (yet)"
-    if len(GBANNED_LIST) > 4095:
-        with io.BytesIO(str.encode(GBANNED_LIST)) as out_file:
-            out_file.name = "Gbannedusers.text"
-            await event.client.send_file(
-                event.chat_id,
-                out_file,
-                force_document=True,
-                allow_cache=False,
-                caption="Current Gbanned Users",
-                reply_to=event,
-            )
-            await event.delete()
-    else:
         await event.edit(GBANNED_LIST)
 
   
